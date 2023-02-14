916f262de40cbfd895ae99176ba6a39a704072e4
yuki-o1o5 <jkksst1712@gmail.com>
2023-02-13 17:27:46 -0800

    modify

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const newArray = [];
-let globalMovieList = [];
-let globalTvList = [];
+let globalAllMovieList = [];
 let globalCheckedMovieId = [];
-let globalCheckedTvId = [];
-
 const checkBoxNameMovie = document.getElementsByName("genreMovie");
 const checkBoxNameTv = document.getElementsByName("genreTv");
 
@@ -31,18 +28,13 @@ const fetchGenreMovieAndTvList = async () => {
     `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=${language}`
   );
   const recievedMovieGenreData = await genreMovieList.json();
-  globalMovieList = recievedMovieGenreData.genres; //array of object
+  globalAllMovieList = recievedMovieGenreData.genres; //array of object
 
-  let genreTvList = await fetch(
-    `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}&language=${language}`
-  );
-  const recievedTvGenreData = await genreTvList.json();
-  globalTvList = recievedTvGenreData.genres; //array of object
   fetchTrendingContents();
 };
 
-const showGenreContent = (secondaryContainer, tertiaryContainer) => {
-  globalMovieList.map((movieGenre) => {
+const showGenreContent = (secondaryContainer) => {
+  globalAllMovieList.map((movieGenre) => {
     let label = document.createElement("label");
     let input = document.createElement("input");
     input.type = "checkbox";
@@ -56,39 +48,24 @@ const showGenreContent = (secondaryContainer, tertiaryContainer) => {
 
     secondaryContainer.appendChild(label);
   });
-
-  globalTvList.map((tvGenre) => {
-    let label = document.createElement("label");
-    let input = document.createElement("input");
-    input.type = "checkbox";
-    input.name = "genreTv";
-    input.value = tvGenre.id;
-    let span = document.createElement("span");
-    span.textContent = tvGenre.name;
-    input.setAttribute("id", tvGenre.id);
-
-    input.addEventListener("change", filterGenre);
-
-    label.append(input, span);
-    tertiaryContainer.appendChild(label);
-  });
 };
 
 const fetchTrendingContents = async (search) => {
   let { apiKey } = configuration;
   try {
     const fethData = await fetch(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
     );
     const recievedData = await fethData.json();
 
-    let finalDataArray = recievedData.results;
+    let finalDataArray = recievedData.results; //array of object
 
-    let newArray = makeNewArray(finalDataArray);
+    let newArray = makeNewArray(finalDataArray); // array of object
 
     let chekedFilter = [];
     if (globalCheckedMovieId.length > 0) {
       chekedFilter = newArray.filter(({ genreId }) => {
+        //?? genre Id
         return isAllIncludes(globalCheckedMovieId, genreId);
       });
     } else {
@@ -116,34 +93,25 @@ const fetchTrendingContents = async (search) => {
 
 const makeContents = (array) => {
   let container = document.getElementById("container");
+
   let nav = document.getElementById("nav");
   nav.innerHTML = "";
-  let searchbar = document.getElementById("search-bar");
 
+  let searchbar = document.getElementById("search-bar");
   let searchPrimaryContainer = document.getElementById(
     "search-primary-container"
   );
   let searchSecondaryContainer = document.getElementById(
     "search-secondary-container"
   );
-  let searchTertiaryContainer = document.getElementById(
-    "search-tertiary-container"
-  );
 
   if (!searchPrimaryContainer.hasChildNodes()) {
     searchbar.innerHTML = "";
-    searchPrimaryContainer.innerHTML = "";
-    searchSecondaryContainer.innerHTML = "";
-    searchTertiaryContainer.innerHTML = "";
 
-    showGenreContent(searchSecondaryContainer, searchTertiaryContainer);
+    showGenreContent(searchSecondaryContainer);
     let searchControl = document.createElement("div");
     let account = document.createElement("div");
-    searchbar.append(
-      searchPrimaryContainer,
-      searchSecondaryContainer,
-      searchTertiaryContainer
-    );
+    searchbar.append(searchPrimaryContainer, searchSecondaryContainer);
     searchControl.classList.add("search-control");
     searchControl.innerHTML =
       "<input id= 'search-input'></input> <button class= 'search-button' onclick= fetchDataSearch(event)>search</button>";
@@ -153,20 +121,36 @@ const makeContents = (array) => {
 
     searchPrimaryContainer.append(searchControl, account);
   }
+
   let main = document.getElementById("main");
   main.innerHTML = "";
 
   // nav
   let menu = document.createElement("div");
+
   let navComponent = document.createElement("div");
   let navNameOne = document.createElement("div");
+  let watchList = document.createElement("div");
+  let favolites = document.createElement("div");
+  let bestAction = document.createElement("div");
+  let bestSF = document.createElement("div");
+  let bestRomance = document.createElement("div");
+  let bestMystery = document.createElement("div");
   menu.classList.add("menu");
   menu.textContent = "Movie Search";
   navComponent.classList.add("nav-component");
-  navComponent.textContent = "This is the nav component";
+  navComponent.textContent = "My Movies";
+  watchList.textContent = "Watch List";
+  favolites.textContent = "Favolites";
+  navComponent.append(watchList, favolites);
   nav.append(menu, navComponent);
   navNameOne.classList.add("nav-name");
-  navNameOne.textContent = "My Favolite";
+  navNameOne.textContent = "My Playlists";
+  bestAction.textContent = "Best Action";
+  bestSF.textContent = "Best SF";
+  bestRomance.textContent = "Best Romance";
+  bestMystery.textContent = "Best Mystery";
+  navNameOne.append(bestAction, bestMystery, bestRomance, bestSF);
   navComponent.appendChild(navNameOne);
 
   //main
@@ -184,7 +168,9 @@ const makeContents = (array) => {
       "https://via.placeholder.com/300";
     movieTextControl.classList.add("movie-text-control");
     movieTitle.textContent = array[i].title || array[i].name;
-    movieType.textContent = array[i].type;
+    movieTitle.classList.add("movie-title");
+    changeId(array[i].genreId, movieType);
+    movieType.classList.add("movie-type");
     moviediscription.textContent = array[i].overView;
     main.appendChild(movieControl);
     movieControl.append(movieImg, movieTextControl);
@@ -202,18 +188,24 @@ const fetchDataSearch = async () => {
   fetchTrendingContents(inputValue);
 };
 
+const changeId = (array, text) => {
+  let sample = [];
+  array.forEach((item) => {
+    globalAllMovieList.forEach((genre) => {
+      if (item === genre.id) {
+        sample.push(genre.name);
+      }
+    });
+    text.textContent = sample;
+  });
+};
+
 const filterGenre = async () => {
   globalCheckedMovieId = [];
   checkBoxNameMovie.forEach((element) => {
+    console.log(element);
     if (element.checked) {
       globalCheckedMovieId.push(parseInt(element.value));
-    }
-  });
-
-  globalCheckedTvId = [];
-  checkBoxNameTv.forEach((element) => {
-    if (element.checked) {
-      globalCheckedTvId.push(parseInt(element.value));
     }
   });
 
